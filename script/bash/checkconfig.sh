8503dd37b70107eacf7bf872735ece4e2ab1976b
keda_dcj <dangchaojin@kedacom.com>
2020-04-15 20:49:06 +0800

    fix

--- script/bash/checkconfig.sh
+++ script/bash/checkconfig.sh
@@ -4,13 +4,13 @@
 echo "execution script: $0"
 
 checkFun(){
-    ## check number of parameter, exit if parameter error
-    if test $# -ne 3
+    ## check number of parameter
+    if test $# -ne 4
     then
         echo "param num is: $#, error! so return!"
         return 1
     fi
-    echo "Start Checking filename:$1  season:$2  checkstr:$3"
+    echo "Start Checking filename:$1  section:$2  checkstr:$3  addstr:$4"
 
     ## check file exists?
     if ! test -e $1   ##test test in redhat
@@ -21,13 +21,14 @@ checkFun(){
         touch $1
     fi
 
-    ## check season exist?
+    ## check section exists?
     if [ $(grep -c "\[$2\]" $1) -eq 0 ];
     then
-        echo "season: [$2] does not exist in file, now add to file."
-        echo "\n[$2]" >> $1     ##  -e
+        echo "section: [$2] does not exist in file, now add to file."
+		echo -e "" >> $1
+        echo -e "[$2]" >> $1          ##根据系统不同，有的可以直接用\n
         ##printf "\n[$2]\n" >> $1
-        sed -i "/\[$2\]/a\\$3" $1
+        sed -i "/\[$2\]/a\\$4" $1
     else
         strTmp=$3
         echo "[$2] already in file, check checkstr:${strTmp% *}."
@@ -36,21 +37,23 @@ checkFun(){
         echo "[$2] already in file, check checkstr:${strTmp%%=*}."
 
         ##if [ $(grep -c " *${strTmp%%=*} *=" $1) -eq 0 ];   ##%% | %
-        if [ $(grep -c "\<${strTmp%% *}" $1) -eq 0 ];
+        ##if [ $(grep -c "\<${strTmp%% *}" $1) -eq 0 ];
+        if [ $(grep -c "$3" $1) -eq 0 ];
         then
-            echo "season:[$2] need add str: "$3""
-            sed -i "/\[$2\]/a\\$3" $1
+            echo "section:[$2] need add str: "$3""
+            sed -i "/\[$2\]/a\\$4" $1
         fi
     fi
 
     return 0
 }
 
-## format: checkFun  fileName  seasonName  checkStr
-checkFun "/mnt/hgfs/test/pas.ini" "HcmInfo" "add = add1"
-checkFun "/mnt/hgfs/test/pas.ini" "dcj"     "adddcj = adddcj1"
+##format:         fileName               sectionName   addStr   checkStr
+checkFun "/opt/mcu/pas/conf/pasdebug.ini" "HcmInfo"   "add"   "add = add1"
+checkFun "/opt/mcu/pas/conf/pasdebug.ini" "dcj"       "dcj"   "dcj = cj1"
+checkFun "/opt/mcu/pas/conf/spcfg.ini" "HcmInfo"   "add"   "add = add1"
 
-echo "return value: $?"
+##echo "return value: $?"
 
 
 ##test step:
