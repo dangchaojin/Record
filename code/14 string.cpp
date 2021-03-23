180e70af3d189b7dc03622a40468baa9559d5731
dangchaojin <15001908189@chinasoft.com>
2021-03-23 15:01:05 +0800

    按字符分割

--- code/14 string.cpp
+++ code/14 string.cpp
@@ -4,12 +4,29 @@
 
 int main()
 {
-	//按特定字符分割
+	//按特定字符分割1:
     std::stringstream strStream("112,119,,,110");
     std::string tempStr("");
     while (getline(strStream, tempStr, ',')) {
         std::cout << tempStr << std::endl;
     }
+	
+	
+	//按特定字符分割2:
+    std::string str("dasd,sda ,da.   sad");
+    std::regex re(",");
+    std::vector<std::string> ss = std::vector<std::string>{std::sregex_token_iterator(str.begin(), str.end(), re, -1),
+                                                 std::sregex_token_iterator()};
+												 
+	//按特定字符分割3:(需要使用string.h)											 
+    char str[] = "hello world look";
+    char *p;
+    p = strtok(str, " ");
+    while (p) {
+        std::cout << p << std::endl;
+        p = strtok(NULL, " ");
+    }
+	
 	
 	//
 	std::string findStr("112,119,,,110");
