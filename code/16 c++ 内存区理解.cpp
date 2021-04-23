5cb6ae9bac98501e2572fd024f193cf9deff704e
dangchaojin <15001908189@chinasoft.com>
2021-04-23 15:37:46 +0800

    add 16 c++11

---
+++ code/16 c++ 内存区理解.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+class AA {
+public:
+    AA();
+    ~AA();
+    void Display();
+};
+
+AA::AA()
+{
+    cout << "AA." << endl;
+}
+AA::~AA()
+{
+    cout << "~AA." << endl;
+}
+void AA::Display()
+{
+    cout << "display a:" << endl;
+}
+
+int main()
+{
+    shared_ptr<AA> testA;
+    testA->Display();   // 应该指向代码区？所以运行不会报错？而有成员的时候会报错呢？
+    cout << "test1" << endl;
+
+    if (testA == nullptr) {
+        testA = make_shared<AA>();
+    }
+
+    testA->Display();
+    return 0;
+}
