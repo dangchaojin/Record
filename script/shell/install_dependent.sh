69d5ebb966f0ec0e9125fb014183691ae84faeed
dangchaojin <15001908189@chinasoft.com>
2021-06-04 20:17:57 +0800

    add install_dep.sh

---
+++ script/shell/install_dependent.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 1. change source
+
+# 2. sudo passwd root;
+
+echo "1" | sudo -S apt-get update;
+
+echo "1" | sudo -S apt-get install vim;
+
+echo "1" | sudo -S apt-get install git;
+
+echo "1" | sudo apt-get install cloc;
